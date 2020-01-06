d9eee6a344feabed4c6ec54d33fd60be0a3841ac
Richard Manning <xprograming@gmx.co.uk>
2020-01-06 06:15:37 +0000

    Update slots.js

--- slots.js
+++ slots.js
@@ -1,92 +1,153 @@
-function Slotmachine(reelsEl, triggerEl, resultEl, opts = {}) {
-  this.reelsEl = reelsEl;
-  this.triggerEl = triggerEl;
-  this.resultEl = resultEl;
-
-  // row to be used to calculate game result (zero indexed)
-  this.resultRow = opts.resultsRow ||  1;
-  // base amount of time each reel will spin
-  this.baseTime =  opts.baseTime || 2000;
-  // flag so we don't start a new game if current one isn't finished
-  this.isSpinning = false;
-  // keep track of our reels timers
-  this.timers = [];
-  // keep track of currently selected tile values
-  this.selectedTiles = [];
-  // speed of tile movement
-  this.speed = opts.speed || 200;
-
-  this.bindButton();
-}
-
-Slotmachine.prototype.bindButton = function() {
-  let self = this;
-  this.triggerEl.addEventListener('click', function() {
-    if (!self.isSpinning) {
-      self.newGame();
-    }
-  });
-};
-
-Slotmachine.prototype.spin = function(reel, reelNum) {
-  // take the last tile (we do this dom selection separate because querySelectorAll isn't a live list)
-  let lastTile = reel.querySelector('li:last-child');
-  // put it at the beginning
-  reel.prepend(lastTile);
-  let tiles = reel.querySelectorAll('li');
-  // keep track of our new selected value
-  this.selectedTiles[reelNum] = tiles[this.resultRow].getAttribute('data-result');
-};
-
-Slotmachine.prototype.calculateWin = function() {
-  let targetResult = this.selectedTiles[0];
-  let currResult;
-
-  // compare each successive reel's result to the first reel's result
-  for (let i = 1; i < this.selectedTiles.length; i++) {
-    currResult = this.selectedTiles[i];
+class RandomPicker extends React.PureComponent {
+  constructor() {
+    super();
+    
+    this.state = {
+      isRunning: false,
+      currentChoice: ''
+    };
+    
+    this.interval = null;
+    this.intervalDuration = 25;
+    this.duration = 1000;
+    
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
+    this.pickChoice = this.pickChoice.bind(this);
+    this.setChoice = this.setChoice.bind(this);
   }
-};
-
-Slotmachine.prototype.newGame = function() {
-  // reset result & values
-  let rand;
-  let timeToSpin = this.baseTime;
-  let self = this;
-
-  this.isSpinning = true;
-
-  // Start spinning each reel
-  this.reelsEl.forEach(function(reel, index) {
-    let timer = setInterval(function() {
-        self.spin(reel, index);
-    }, self.speed);
-    self.timers[index] = timer;
-  });
-
-  // Clear each timer to stop reels spinning
-  for (let i = 0; i < this.timers.length; i++) {
-    // random amount of time (0 to 1 second) in between each stop
-    rand = Math.floor(Math.random()*1000);
-    // total spin time is previous reels total time plus rand
-    timeToSpin += rand;
-    // stop each reel
-    setTimeout(function(i) {
-      clearInterval(self.timers[i]);
-      // calculate result on last reel
-      if (i === self.timers.length-1) {
-        self.isSpinning = false;
+  
+  start() {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.setChoice, this.intervalDuration);
+    this.setState({ isRunning: true });
+    setTimeout(() => {
+      if (this.state.isRunning) {
+        this.stop()
       }
-    }, timeToSpin, i);
+    }, this.duration);
   }
-};
-
-function init() {
-  reelsEl = document.querySelectorAll('.slotmachine__reel');
-  triggerEl = document.querySelector('.slotmachine__handle');
-  resultEl = document.querySelector('.slotmachine__result');
-
-  let caffeineSlots = new Slotmachine(reelsEl, triggerEl, resultEl);
+  
+  stop() {
+    clearInterval(this.interval);
+    this.setState({ isRunning: false });
+  }
+  
+  reset() {
+    clearInterval(this.interval);
+    this.setState({ isRunning: false, currentChoice: '' });
+  }
+  
+  pickChoice() {
+    const { items } = this.props;
+    const choice = items[Math.floor(Math.random() * items.length)];
+    return choice;
+  }
+  
+  setChoice() {
+    this.setState({ currentChoice: this.pickChoice() })
+  }
+  
+  render() {
+    const { isRunning, currentChoice } = this.state;
+    
+    return (
+      <div className="RandomPicker">
+        <RandomPickerChoice choice={currentChoice} />
+        <RandomPickerControls 
+          isRunning={isRunning}
+          hasChoice={currentChoice.trim().length > 0}
+          start={this.start}
+          stop={this.stop}
+          reset={this.reset}
+        />
+      </div>
+    );
+  }
 }
 
-document.addEventListener('DOMContentLoaded', init);
+RandomPicker.propTypes = {
+  items: PropTypes.array,
+  duration: PropTypes.number
+};
+
+class RandomPickerChoice extends React.PureComponent {
+  render() {
+    const { choice } = this.props;
+    const content = choice.trim().length > 0 ? choice : '?';
+    
+    return (
+      <div className="RandomPicker__choice">
+        <span className="RandomPicker__choiceItem">{content}</span>
+      </div>  
+    );
+  }
+}
+
+RandomPickerChoice.propTypes = {
+  choice: PropTypes.string
+};
+
+class RandomPickerControls extends React.PureComponent {
+  render() {
+    const { 
+      isRunning, 
+      hasChoice, 
+      start, 
+      stop, 
+      reset 
+    } = this.props;
+    
+    return (
+      <div className="RandomPicker__controls">
+        <button 
+          class={`RandomPicker__button ${isRunning && 'RandomPicker__button--stop'}`}
+          onClick={isRunning ? stop : start}
+        >
+          {isRunning ? 'stop' : 'start'}
+        </button>
+        <button 
+          disabled={isRunning || !hasChoice} 
+          class="RandomPicker__button RandomPicker__button--reset" 
+          onClick={reset}
+        >
+          reset
+        </button>
+      </div>
+    );
+  }
+}
+
+RandomPickerControls.propTypes = {
+  isRunning: PropTypes.bool,
+  hasChoice: PropTypes.bool,
+  start: PropTypes.func,
+  stop: PropTypes.func,
+  reset: PropTypes.func,
+};
+
+const namesList = [
+	'Marcelo',  
+	'Lizzette',  
+	'Pauline',  
+	'Fumiko',  
+	'Tomasa',  
+	'Bertha',  
+	'Antoinette',  
+	'Tianna',  
+	'Ammie',  
+	'Victorina',  
+	'Marlon',  
+	'Jules',  
+	'Arletha',  
+	'Ellyn',  
+	'Karol',  
+	'Corrin', 
+	'Josephine',
+];
+
+ReactDOM.render(
+  <RandomPicker items={namesList} />,
+  document.getElementById('random-picker')
+);
